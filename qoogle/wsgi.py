c6bd25aef6944fa7ea04fa40b55571a0cb7da906
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-19 17:03:14 +0430

    update gitignore

--- qoogle/wsgi.py
+++ qoogle/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qoogleaa project.
+WSGI config for qoogle project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qoogleaa.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qoogle.settings')
 
 application = get_wsgi_application()
