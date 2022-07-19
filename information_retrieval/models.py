2a615a45588ab60aed27e0014b3148667529ff86
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-19 17:21:57 +0430

    add models

--- information_retrieval/models.py
+++ information_retrieval/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from model_utils.models import TimeStampedModel
 
-# Create your models here.
+
+class Query(TimeStampedModel):
+    text = models.CharField(max_length=511)
+    engine = models.PositiveSmallIntegerField(default=0)
+
+
+class Response(TimeStampedModel):
+    verse = models.CharField(max_length=511)
+    verse_number = models.CharField(max_length=511)
+    surah_name = models.CharField(max_length=127)
+
