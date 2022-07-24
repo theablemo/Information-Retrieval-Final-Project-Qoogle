b52ce45fc0d844fe0f229dac5487789bb20ec861
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-24 14:51:38 +0430

    add search/ endpoint

--- information_retrieval/urls.py
+++ information_retrieval/urls.py
@@ -4,4 +4,5 @@ from information_retrieval import views
 
 urlpatterns = [
     path('', views.search_query, name="search_query"),
+    path('search/', views.search_results, name="search_results"),
 ]
