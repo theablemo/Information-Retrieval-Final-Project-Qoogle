0236a2d9f964d195290ae01ade34fc2990a7a5b3
Mohammad Amin <m.agasemzadeh@gmail.com>
2022-07-26 19:40:54 +0430

    add classification to ui

---
+++ information_retrieval/lib/classification_handler.py
@@ -0,0 +1,17 @@
+class ClassificationHandler:
+
+    def get_surah_predication_v1(self, surah_number, verse_number):
+        return "surah_predication_v1"
+
+    def get_surah_predication_v2(self, surah_number, verse_number):
+        return "surah_predication_v2"
+
+    def get_makki_madani_predication(self, surah_number):
+        return "مکی"
+
+    def get_makki_madani_real(self, surah_number):
+        return "مکی"
+
+    def get_four_cluster_type(self, surah_number):
+        return "کلاستر ۱"
+
