a8d3d3c09df069650db1a5bdea9abe6767bf2cc0
Aryan Ahadinia <62179268+AryanAhadinia@users.noreply.github.com>
2022-07-27 20:05:48 +0430

    Add Lucky

--- information_retrieval/urls.py
+++ information_retrieval/urls.py
@@ -4,5 +4,7 @@ from information_retrieval import views
 
 urlpatterns = [
     path('', views.search_query, name="search_query"),
+    path('lucky/', views.lucky_query, name="lucky_query"),
     path('search/', views.search_results, name="search_results"),
+    path('lucky/search', views.lucky_results, name="lucky_results"),
 ]
--- information_retrieval/views.py
+++ information_retrieval/views.py
@@ -1,17 +1,14 @@
 from django.shortcuts import render
 
-from information_retrieval.forms import QueryForm
-from information_retrieval.lib.classification_handler import ClassificationHandler
 from information_retrieval.lib.quran_mir.Query_expansion import QueryExpansion
 from information_retrieval.models import Query
 from qoogle.settings import BASE_URL
 
+query_expansion = QueryExpansion()
+
 
 def search_query(request):
     return render(request, 'search_bar.html')
-
-
-query_expansion = QueryExpansion()
 
 
 def search_results(request):
@@ -26,6 +23,33 @@ def search_results(request):
     expanded_query = query_expansion.expand_query(text)
 
     return render(request, 'search_results.html', {
+        'text': text,
+        'results': results,
+        'base_url': BASE_URL,
+        'engine': str(engine),
+        # 'cluster': cluster,
+        # 'classification_1': classification_1,
+        # 'classification_2': classification_2,
+        'expanded_query': expanded_query,
+    })
+
+
+def lucky_query(request):
+    return render(request, 'lucky_bar.html')
+
+
+def lucky_results(request):
+    text = request.GET['text']
+    engine = int(request.GET['engine'])
+    query, _ = Query.objects.get_or_create(text=text, engine=engine)
+    query.process()
+    results = list(query.responses.order_by('rank'))
+    # cluster = None  # TODO: retrieve cluster
+    # classification_1 = None  # TODO: retrieve classification type 1
+    # classification_2 = None  # TODO: retrieve classification type 2
+    expanded_query = query_expansion.expand_query(text)
+
+    return render(request, 'lucky_results.html', {
         'text': text,
         'results': results,
         'base_url': BASE_URL,
